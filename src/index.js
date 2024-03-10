3d801c2ef306d36a9f330dc1ef8e63429eb9cf3c
KRitchie823 <158782034+KRitchie823@users.noreply.github.com>
2024-03-10 19:21:38 -0400

    Added basic search engine
    No API at this point

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-alert("hello world");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let currentCityElement = document.querySelector("#current-city");
+  currentCityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
