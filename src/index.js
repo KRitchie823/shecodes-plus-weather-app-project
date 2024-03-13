9d3cff098f75c7a7d008fa8977dfe831765f237c
KRitchie823 <158782034+KRitchie823@users.noreply.github.com>
2024-03-13 18:44:56 -0400

    Added functionality to display weather forecast using API

--- src/index.js
+++ src/index.js
@@ -56,29 +56,39 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "798dc9fa0390f84ceb8a41oa8b735ct6";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
   axios(apiUrl).then(displayForecast);
 }
 
-function displayForecast() {
-  let days = ["Wed", "Thu", "Fri", "Sat", "Sun"];
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">⛅</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `<div class="weather-forecast-day">
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+      <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
     <div class="weather-forecast-temps">
         <div class="weather-forecast-temp">
-            <strong>15°</strong>
+            <strong>${Math.round(day.temperature.maximum)}°</strong>
         </div>
-        <div class="weather-forecast-temp">9°</div>
+        <div class="weather-forecast-temp">${Math.round(
+          day.temperature.minimum
+        )}°</div>
     </div>    
   </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
